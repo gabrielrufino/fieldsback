45c27d0a917acdec8cc1557124c7027268c9675a
Gabriel Rufino <gjscr@cin.ufpe.br>
2018-01-01 07:03:36 -0200

    public (index.html) with a basic form and new module (fs)

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const fs = require('fs')
 
 const app = express()
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-	res.send('fieldsback ON!')
+	fs.readFile('public/index.html', 'utf8', (err, data) => {
+		if (err)
+			res.send(err)
+		else
+			res.send(data)
+	})
+})
+
+app.post('/thanks', (req, res) => {
+	res.send(":D")
 })
 
 const port = process.env.PORT || 3000
