e81848d4d2113dc1af52dfe0482f0755d3bd6629
Gabriel Rufino <contato@gabrielrufino.com>
2019-03-04 20:27:55 -0300

    Adapting db.js code to the new database environment variables

--- db.js
+++ db.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose')
 const db       = mongoose.connection
 
-const database = process.env.DATABASE || 'mongodb://localhost/fieldsback'
-mongoose.connect(database)
+const {
+  DB_USER,
+  DB_PASS,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME
+} = process.env
+
+const databaseUrl = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
+mongoose.connect(databaseUrl)
 
 db.on('error', console.error.bind(console, 'connection error:'))
 
